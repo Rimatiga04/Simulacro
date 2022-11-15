7d02341e097929df42a6625ea73c2b7f94dc9f6b
Usuario <Usuario@LAPTOP-02T536GT>
2022-11-15 20:56:17 +0100

    Commit inicial Subimos el archivo a Github

---
+++ Ejercicios/src/EJERCICIOS.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EJERCICIOS {
+
+	public static void main(String[] args) {
+	 int b=8;
+	 int c=0;
+	 int d=7;
+	 
+	 d++;
+	 b=b-d;
+	 c=b/c;
+	 System.out.println("El resultado es "+c);
+	 }
+	 
+	}
