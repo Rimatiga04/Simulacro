476acadde4624bcd5ae32293afaa9bc1d853102d
Usuario <Usuario@LAPTOP-02T536GT>
2022-11-15 20:59:59 +0100

    Commitinicial Se sube el archivo

---
+++ ejemplo_entornos/src/ejemplo_entornos/Principal.java
@@ -0,0 +1,18 @@
+package ejemplo_entornos;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	 int b=8;
+	 int c=0;
+	 int d=7;
+	 
+	 d++;
+	 b=b-d;
+	 c=b/c;
+	 System.out.println("El resultado es "+c);
+	 }
+	 
+	}
